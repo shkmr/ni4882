efc571bfdb7d35c3aef925e85a15eafd32bf721a
Shigenobu Kimura <skimu@me.com>
2019-05-05 20:27:33 -0500

    initial import

---
+++ ni4882.h
@@ -0,0 +1,24 @@
+/*
+ * ni4882.h
+ */
+
+/* Prologue */
+#ifndef GAUCHE_NI4882_H
+#define GAUCHE_NI4882_H
+
+#include <gauche.h>
+#include <gauche/extend.h>
+
+SCM_DECL_BEGIN
+
+/*
+ * The following entry is a dummy one.
+ * Replace it for your declarations.
+ */
+
+extern ScmObj test_ni4882(void);
+
+/* Epilogue */
+SCM_DECL_END
+
+#endif  /* GAUCHE_NI4882_H */
---
+++ ni4882.scm
@@ -0,0 +1,19 @@
+;;;
+;;; ni4882
+;;;
+
+(define-module ni4882
+  (export test-ni4882 ;; dummy
+          )
+  )
+(select-module ni4882)
+
+;; Loads extension
+(dynamic-load "ni4882")
+
+;;
+;; Put your Scheme definitions here
+;;
+
+
+
