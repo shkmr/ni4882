97be0a9b823cf26dbd2e6b434b2f689a54005088
Shigenobu Kimura <skimu@me.com>
2019-06-06 21:45:53 -0500

    some test

--- test.scm
+++ test.scm
@@ -3,11 +3,12 @@
 ;;;
 
 (use gauche.test)
-
 (test-start "ni4882")
 (use ni4882)
-
 (test-module 'ni4882)
+(test* "UNL" #x3f UNL)
+(test* "UNT" #x5f UNT)
+(test* "ibfindA"  #t (procedure? ibfindA))
 (test-end :exit-on-failure #t)
 
 ;;; EOF
