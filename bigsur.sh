9179ee5bcc2b429408be7cd95fe0e225aa2070d1
Shigenobu Kimura <skimu@me.com>
2021-02-25 19:45:11 -0600

    bigsur.sh: added more apps

--- bigsur.sh
+++ bigsur.sh
@@ -5,9 +5,13 @@
 ##    https://knowledge.ni.com/KnowledgeArticleDetails?id=kA03q000000YIJsCAO&l=en-US
 ##
 for i in com.ni.ni4882.gpibExplorer \
+         com.ni.ni4882.gpibtsw      \
+         com.ni.framework.DriverWizard \
+         'com.ni.framework.NI-VISA Configuration' \
+         com.ni.framework.NIvisaic  \
          com.ni.framework.NIIOTrace 
 do
     echo "Writing defaults to $i"
-    defaults write $i NSGraphicsContextAllowOverRestore -bool YES
-    defaults write $i NSViewAllowsRootLayerBacking -bool NO
+    defaults write "$i" NSGraphicsContextAllowOverRestore -bool YES
+    defaults write "$i" NSViewAllowsRootLayerBacking -bool NO
 done
